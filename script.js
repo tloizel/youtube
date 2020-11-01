c2c92ec01e66dbe7c0eb4a290cbad9d0f2632f21
tloizel <thomas.loizel@gmail.com>
2020-11-01 18:23:42 +0100

    editP

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var rangeIdea = 1; //value of Qt on range - 1
 var ideaQl =  5; //value of Ql on range - 5
 var ideasQt = 0; //amount of ideas ready to edit - 0
 var ideasQtTotal = 0; //amount of ideas since beginning - 0
-var ideaSpeed = 60000; //speed of idea generation : the lower the number the faster ideas generate
+var ideaSpeed = 6000; //speed of idea generation : the lower the number the faster ideas generate
 var energy = 100; //current energy amount
 var energyMax = 100; //max energy amount
 var batchEnergyCost = 25; //cost of each batch of ideas
@@ -34,7 +34,7 @@ var shootEditRem = 200; //number of remaining clicks - 200
 var videosEdited = 0; //number of videos edited - 0
 var videosEditedTotal = 0; //TOTAL number of videos edited - 0
 var computerMemory = 1; //max videos edited - 1
-var editPressSpeed = 50 * editP;//50ms ONLY CHANGED BY PRESTIGE
+var editPressSpeed = 70 * editP;//50ms ONLY CHANGED BY PRESTIGE - LINE 1163 TO CHANGE
 var editorSpeed = 4; //how many times to call the function - 1
 
 //UPLOAD
@@ -43,7 +43,7 @@ var videosUploaded = 0; //Videos online - 0
 var averageQlNum = 0; //average Ql numerator - 0
 var averageQl = 0; //average video quality after upload - 0
 var likeDislikeFactor = 1; //factor used to change LDR directly - 1
-var uploadSpeed = 1; //*100
+var uploadSpeed = 1000; //*100
 var loadState = 0; //load state of progress bar
 var autoUploadActivated = false; //0 for no 1 for yes
 
@@ -1160,7 +1160,7 @@ function load() {
   //prestige changes repetition because variables init come before load()
   creativity = 1 + creativityP;
   energyRegenSpeed = 1000 * energyP;
-  editPressSpeed = 50 * editP;
+  editPressSpeed = 70 * editP;//EDITPRESSSPEED TO BE CHANGED HERE
 }
 
 function deleteLocalStorage() {
